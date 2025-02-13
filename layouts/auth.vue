82c8657c4a69ceda50fed957a2ccf5797bf88c20
BrayaCheat <prayacheat@gmail.com>
2025-02-13 11:45:43 +0700

    feat: improve session

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -9,5 +9,14 @@
 
 <script setup>
   import { Toaster } from '@/components/ui/toast';
+  import { useSessionStore } from '@/store/session';
+
+  const sessionStore = useSessionStore()
+  //watcher
+  watch(() => sessionStore.session, (val) => {
+    if(val){
+      return navigateTo('/')
+    }
+  }, {immediate: true, deep: true})
 </script>
 
