0e11d23875c68d3ca0f00f8f5d726332c39bfea7
BrayaCheat <prayacheat@gmail.com>
2025-02-13 11:56:26 +0700

    refactor: format

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,22 +1,21 @@
 <template>
   <ClientOnly>
     <div class="relative h-screen">
-      <NuxtPage/>
-      <Toaster/>
+      <NuxtPage />
+      <Toaster />
     </div>
   </ClientOnly>
 </template>
 
 <script setup>
-  import { Toaster } from '@/components/ui/toast';
-  import { useSessionStore } from '@/store/session';
+import { Toaster } from '@/components/ui/toast';
+import { useSessionStore } from '@/store/session';
 
-  const sessionStore = useSessionStore()
-  //watcher
-  watch(() => sessionStore.session, (val) => {
-    if(val){
-      return navigateTo('/')
-    }
-  }, {immediate: true, deep: true})
+const sessionStore = useSessionStore()
+//watcher
+watch(() => sessionStore.session, (val) => {
+  if (val) {
+    return navigateTo('/')
+  }
+}, { immediate: true, deep: true })
 </script>
-
