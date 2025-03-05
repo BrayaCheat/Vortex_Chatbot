6d606ed5dd8e8897a17751abf151030862e1c459
BrayaCheat <prayacheat@gmail.com>
2025-03-05 14:17:10 +0700

    feat: apply feedback report

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,9 +2,11 @@
   <nav class="flex items-center top-0 inset-x-0 z-50 border-b px-3 shadow-sm">
     <h1 class="font-semibold text-xl py-3 flex-1">{{ new Date().toDateString() }}</h1>
     <ClearChat />
+    <Report class="bg-yellow-100 border border-yellow-300 text-yellow-500"/>
   </nav>
 </template>
 
 <script setup>
 import ClearChat from '@/components/ClearChat.vue';
+import Report from '@/components/Report.vue';
 </script>
---
+++ components/Report.vue
@@ -0,0 +1,10 @@
+<template>
+  <a href="mailto:prayacheat@gmail.com?subject=Bugs-Report&body=Leave your message here...">
+    <component :is="Bug" />
+  </a>
+</template>
+
+<script setup>
+import { Button } from '@/components/ui/button';
+import { Bug } from 'lucide-vue-next';
+</script>
